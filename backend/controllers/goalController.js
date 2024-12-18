c32577c3d8adda2e81729b52d208a71b89957929
BeygonK <kipkoechbeygon@gmail.com>
2024-12-18 09:21:27 +0300

    set controllers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,31 +1,32 @@
- 
+const asyncHandler = require('express-async-handler')
+
 // @desc    GET goals
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler (async (req, res) => {
     res.status(200).json({messsage: 'Get goals'})
-}
+})
 
 // @desc    POST goals
 // @route   POST /api/goals
 // @access  Private
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async  (req, res) => {
     res.status(200).json({messsage: 'Get goals'})
-}
+})
 
 // @desc    PUT Goals
 // @route   PUT /api/goals/id
 // @access  Private
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
     res.status(200).json({messsage: 'Get goals'})
-}
+})
 
 // @desc     DELETE Goals
 // @route    DELETE /api/goals/id
 // @access   Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
     res.status(200).json({messsage: 'Get goals'})
-}
+})
 
 
 
